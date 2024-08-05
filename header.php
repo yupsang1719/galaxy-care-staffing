0116534f0a736d0996374809dbb1d0a7ee7e5215
yupsang1719 <yupsang1719@outlook.com>
2024-08-05 10:57:59 +0100

    Add header.php file with all the codes

---
+++ header.php
@@ -0,0 +1,12 @@
+<header>
+    <nav>
+        <ul>
+            <li><a href="#home">Home</a></li>
+            <li><a href="#about">About Us</a></li>
+            <li><a href="#employers">Employers</a></li>
+            <li><a href="#registration">Employee Registration</a></li>
+            <li><a href="#staff">Staff Area</a></li>
+            <li><a href="#contact">Contact Us</a></li>
+        </ul>
+    </nav>
+</header>
