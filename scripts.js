05e66ceb0847a3c1d7127bd02381a5df433d88cc
yupsang1719 <yupsang1719@outlook.com>
2024-08-05 11:21:23 +0100

    Updated scripts.js: Configured Swiper to autoplay.

--- scripts.js
+++ scripts.js
@@ -1,13 +1,8 @@
 document.addEventListener('DOMContentLoaded', function () {
     var swiper = new Swiper('.home-slider', {
         loop: true,
-        pagination: {
-            el: '.swiper-pagination',
-            clickable: true,
-        },
-        navigation: {
-            nextEl: '.swiper-button-next',
-            prevEl: '.swiper-button-prev',
+        autoplay: {
+            delay: 5000,
         },
     });
 
